caff2bcb3b0a7883576670c2496e74b4ae795fec
Randgriz11 <jacobtea12@gmail.com>
2023-09-12 13:09:13 -0700

    Highlight

--- js/script.js
+++ js/script.js
@@ -4,6 +4,49 @@ var game = new Chess()
 var $status = $('#status')
 var $fen = $('#fen')
 var $pgn = $('#pgn')
+var whiteSquareGrey = '#18453B'
+var blackSquareGrey = '#4B5320'
+
+
+function removeGreySquares () {
+  $('#myBoard .square-55d63').css('background', '')
+}
+
+function greySquare (square) {
+  var $square = $('#myBoard .square-' + square)
+
+  var background = whiteSquareGrey
+  if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey
+  }
+
+  $square.css('background', background)
+}
+
+function onMouseoutSquare (square, piece) {
+  removeGreySquares()
+}
+
+
+function onMouseoverSquare (square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  })
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return
+
+  // highlight the square they moused over
+  greySquare(square)
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to)
+  }
+}
+
 
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
@@ -15,6 +58,7 @@ function onDragStart (source, piece, position, orientation) {
     return false
   }
 }
+
 
 function onDrop (source, target) {
   // see if the move is legal
@@ -74,6 +118,8 @@ function updateStatus () {
   $status.html(status)
   $fen.html(game.fen())
   $pgn.html(game.pgn())
+
+  
 }
 
 var config = {
@@ -82,8 +128,14 @@ var config = {
   onDragStart: onDragStart,
   onDrop: onDrop,
   onSnapEnd: onSnapEnd,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
   dropOffBoard: 'snapback'
 }
 board = Chessboard('myBoard', config)
+$('#clearBtn').on('click', board.clear)
+
+$('#startBtn').on('click', board.start)
+
 
 updateStatus()
